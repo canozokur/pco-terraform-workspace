9e03488157c57cd36fb51c4e15aba65bbf5a8075
canozokur <13416785+canozokur@users.noreply.github.com>
2022-11-15 01:00:40 +0200

    Create free tier Ampere VM

---
+++ instance.tf
@@ -0,0 +1,40 @@
+data "oci_identity_availability_domains" "all" {
+  compartment_id = var.oci_tenancy_ocid
+}
+
+locals {
+  availability_domains = [for i in data.oci_identity_availability_domains.all.availability_domains : i.name]
+}
+
+resource "random_shuffle" "ad" {
+  input        = local.availability_domains
+  result_count = 1
+}
+
+data "oci_core_images" "img" {
+  compartment_id = var.oci_tenancy_ocid
+
+  operating_system         = local.operating_system
+  operating_system_version = local.operating_system_version
+}
+
+resource "oci_core_instance" "pco-k8s-node" {
+  compartment_id      = var.oci_tenancy_ocid
+  availability_domain = element(random_shuffle.ad.result, 0)
+  shape               = local.shape
+  shape_config {
+    ocpus         = 4
+    memory_in_gbs = 24
+  }
+
+  create_vnic_details {
+    assign_public_ip = true
+
+  }
+
+  source_details {
+    boot_volume_size_in_gbs = 200
+    source_id               = data.oci_core_images.img.id
+    source_type             = "bootVolume"
+  }
+}
---
+++ locals.tf
@@ -0,0 +1,5 @@
+locals {
+  operating_system         = "Ubuntu"
+  operating_system_version = "22.04 Minimal aarch64"
+  shape                    = "VM.Standard.A1.Flex"
+}
