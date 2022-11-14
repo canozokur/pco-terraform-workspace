62b2785fe59f862d11864eb58103b28925a096d3
canozokur <13416785+canozokur@users.noreply.github.com>
2022-11-15 01:31:23 +0200

    Update vcn subnet size

--- locals.tf
+++ locals.tf
@@ -2,6 +2,6 @@ locals {
   operating_system         = "Ubuntu"
   operating_system_version = "22.04 Minimal aarch64"
   shape                    = "VM.Standard.A1.Flex"
-  network                  = "10.0.0.0/8"
-  subnet                   = "10.0.0.0/8"
+  network                  = "10.0.0.0/16"
+  subnet                   = "10.0.0.0/16"
 }
