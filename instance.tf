f970cec3d9187407d6d58e1e9f57fa2742cec655
canozokur <13416785+canozokur@users.noreply.github.com>
2022-11-15 13:15:15 +0200

    Add public keys

--- instance.tf
+++ instance.tf
@@ -59,4 +59,8 @@ resource "oci_core_instance" "pco-k8s-node" {
     source_id               = one(data.oci_core_images.img.images).id
     source_type             = "image"
   }
+
+  metadata = {
+    "ssh_authorized_keys" = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCk/uh6KjUB8wNwLawPdZyJ9AcN0aHjspZuukQcsI773s1ghy5U4Wlhx+FEeUsZzBEcMRGf8bZzyvhb57H8B/lA7W4wz9TfLHAPu2OqE2Lu8anxqebCmjSchvfckefHe7B/J0oUtyhqOyw5+DPvi8HSbIh5/oxQqwweUGl9nqmhLZj883fnGYGsvoSKHmkuh2PpSLZQng8cGGAR7/1wakIR8XWMmDt7k+AYrTuSihJLpgiWgexZ2mTcVuEpJhhi2nC/0lKly4Czvg/EuKHDMocV0dJz8/1CVxlpO7oQvPMx4x1aPZQqUNYqmv60Voc33xM4Qg7dv4I371O/tOHAew7V"
+  }
 }
