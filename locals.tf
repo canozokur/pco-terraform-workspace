4e58441abf730b634bb2a5f61fa966f2e3c7255f
canozokur <13416785+canozokur@users.noreply.github.com>
2022-11-15 11:39:30 +0200

    Update OS name because Oracle..

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  operating_system         = "Ubuntu"
+  operating_system         = "Canonical Ubuntu" # nobody calls it "Canonical Ubuntu", come on Oracle :(
   operating_system_version = "22.04 Minimal aarch64"
   shape                    = "VM.Standard.A1.Flex"
   network                  = "10.0.0.0/16"
