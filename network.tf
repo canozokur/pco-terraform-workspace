1b94296e8f65d5b39fd97021ff884a2982610e95
canozokur <13416785+canozokur@users.noreply.github.com>
2022-11-15 14:07:03 +0200

    Add route table and attach it

--- network.tf
+++ network.tf
@@ -11,10 +11,26 @@ resource "oci_core_internet_gateway" "default" {
   vcn_id = oci_core_vcn.default.id
 }
 
+resource "oci_core_route_table" "default" {
+  compartment_id = oci_identity_compartment.main.id
+  vcn_id         = oci_core_vcn.default.id
+
+  route_rules {
+    destination       = "0.0.0.0/0"
+    destination_type  = "CIDR_BLOCK"
+    network_entity_id = oci_core_internet_gateway.default.id
+  }
+}
+
 resource "oci_core_subnet" "default" {
   compartment_id = oci_identity_compartment.main.id
 
   cidr_block   = local.subnet
   display_name = "default"
   vcn_id       = oci_core_vcn.default.id
+}
+
+resource "oci_core_route_table_attachment" "default" {
+  subnet_id      = oci_core_subnet.default.id
+  route_table_id = oci_core_route_table.default.id
 }
