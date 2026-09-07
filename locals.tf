a9f038b011863cff95d2838be49d7e9b7401e09c
canozokur <13416785+canozokur@users.noreply.github.com>
2026-09-07 16:01:43 +0300

    Update images

--- locals.tf
+++ locals.tf
@@ -3,7 +3,7 @@ locals {
   network = "10.0.0.0/16"
   subnet  = "10.0.0.0/16"
   image_id = {
-    eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaapb65ywn23pl6t6dzc2bk56k4azbvnmsndcr6bfmn5eoei5toc53a"
-    eu-stockholm-1 = "ocid1.image.oc1.eu-stockholm-1.aaaaaaaavni5omi3qljq5umzlymbzxdczn3cmvgnfwb4tdfsls6qyehlv43q"
+    eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaxloe7grzdoamrmsm3eetkg34prf7rjqte5sewidrlfukaiurltta"
+    eu-stockholm-1 = "ocid1.image.oc1.eu-stockholm-1.aaaaaaaaxcefx34xbq2qqbqkzxzmva4jt5aqfd6xdchg7657hfpg7u7evdbq"
   }
 }
