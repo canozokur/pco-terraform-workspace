d781b3f4c0b43e4fd28b9f56e63335b7b0636d97
canozokur <13416785+canozokur@users.noreply.github.com>
2026-09-07 16:41:36 +0300

    Update images to oracle linux

--- locals.tf
+++ locals.tf
@@ -3,7 +3,7 @@ locals {
   network = "10.0.0.0/16"
   subnet  = "10.0.0.0/16"
   image_id = {
-    eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaxloe7grzdoamrmsm3eetkg34prf7rjqte5sewidrlfukaiurltta"
-    eu-stockholm-1 = "ocid1.image.oc1.eu-stockholm-1.aaaaaaaaxcefx34xbq2qqbqkzxzmva4jt5aqfd6xdchg7657hfpg7u7evdbq"
+    eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaawwulrgpbabqztutg53jurdo7l2rciundmbg544hfh7k4t2wcokaq"
+    eu-stockholm-1 = "ocid1.image.oc1.eu-stockholm-1.aaaaaaaayld52ddzfjc4f677rc7f34hiti2xzfc4tip2ikn7l6q6lmmkofna"
   }
 }
